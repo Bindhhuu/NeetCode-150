af2303ada42d79131354522651f90def840ce1ef
Bindhuu <143107119+Bindhhuu@users.noreply.github.com>
2025-09-30 20:15:33 +0530

    Create TopKFrequentElements.py

---
+++ Arrays & Hashing/TopKFrequentElements.py
@@ -0,0 +1,17 @@
+import heapq
+from collections import Counter
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        counter = Counter(nums)
+        heap = []
+
+        for key, val in counter.items():
+            if len(heap) < k:
+                heapq.heappush(heap, (val, key))
+            else:
+                heapq.heappushpop(heap, (val, key))
+            
+        return [h[1] for h in heap]
+
+        #tc: O(n log k)
+        #sc:O(n)
