51873fedbe6698f5f5db769d15e4a46b7ca7e8cf
Bindhuu <143107119+Bindhhuu@users.noreply.github.com>
2025-09-30 20:11:37 +0530

    Create ContainsDuplicate.py

---
+++ Arrays & Hashing/ContainsDuplicate.py
@@ -0,0 +1,11 @@
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        n = set()
+
+        for num in nums:
+            if num in n:
+                return True
+            n.add(num)    
+        return False
+      #TC: O(n)
+      #SC: O(n)
